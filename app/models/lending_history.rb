6317bc9435e5db84e3ae561261df2dcc4334e87e
NexusGKSoftwares <nexusgk3@gmail.com>
2025-02-17 12:30:01 +0300

    `Added validation for returned_at date in LendingHistory model`

--- app/models/lending_history.rb
+++ app/models/lending_history.rb
@@ -1,3 +1,12 @@
 class LendingHistory < ApplicationRecord
   belongs_to :book
+
+  validates :borrower_name, presence: true
+  validate :returned_at_after_borrowed_at
+
+  def returned_at_after_borrowed_at
+    if returned_at.present? && borrowed_at.present? && returned_at < borrowed_at
+      errors.add(:returned_at, "cannot be before borrowed date")
+    end
+  end
 end
