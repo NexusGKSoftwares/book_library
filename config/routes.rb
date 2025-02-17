2edd5f8ecf159954ad00ac047ee5f42a1613b01a
NexusGKSoftwares <nexusgk3@gmail.com>
2025-02-17 12:35:52 +0300

    `Refactored routes in config/routes.rb`

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
-Rails.application.routes.draw do
-  get "lending_histories/new"
-  get "lending_histories/create"
-  get "books/index"
-  get "books/show"
-  get "books/new"
-  get "books/create"
-  get "books/edit"
-  get "books/update"
-  get "books/destroy"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# Rails.application.routes.draw do
+#   get "lending_histories/new"
+#   get "lending_histories/create"
+#   get "books/index"
+#   get "books/show"
+#   get "books/new"
+#   get "books/create"
+#   get "books/edit"
+#   get "books/update"
+#   get "books/destroy"
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+#   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+#   # Can be used by load balancers and uptime monitors to verify that the app is live.
+#   get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
@@ -20,4 +20,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-end
+  Rails.application.routes.draw do
+    resources :books do
+      resources :lending_histories, only: [:new, :create]
+    end
+  
+    resources :lending_histories, only: [] do
+      member do
+        patch :return_book
+      end
+    end
+  
+    root "books#index"
+  end
+  
+
